15f949c3a8d819db6cf12a60c4e0931401149875
Colin <Colin@server>
2012-07-21 16:17:24 -0700

    added test.vm

--- parser.c
+++ parser.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include "parser.h"
 #include "file.h"
+#include "util.h"
 
 // TODO: pass both the asm file and the intermediate file into here
 int parserMain(FileInfoType* fileInfo){
-	printf("hello");
+	debug("asm file: %s, vm file: %s", fileInfo->asmFileName, fileInfo->vmFileName);
+	advance(fileInfo);
 	return 0;
+}
+
+// Get each line from the input vm file
+int advance(FileInfoType* fileInfo){
+	FILE* vmFile = fopen(fileInfo->vmFileName, "r");
+	check_error(vmFile != NULL, "Failed to open vmFile");
+	FILE* asmFile = fopen(fileInfo->asmFileName, "w");
+	check_error(asmFile != NULL, "Failed to open asmFile");
+	char line[128];
+	while (fgets(line, sizeof(line), vmFile) != NULL){
+
+		// TODO: just writing each line directly to asm file for testing
+		fputs(line, asmFile);
+	}
+	fclose(vmFile);
+	fclose(asmFile);
+	return 0;
+error:
+	return 1;
 }
--- parser.h
+++ parser.h
@@ -1,6 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "file.h"
 
+int advance(FileInfoType* fileInfo);
 
 #endif
